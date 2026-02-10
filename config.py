2b13abf8afebeb3d622c4b3a9820885c425dc7ea
Patrick Pinheiro <patrickkaua730@gmail.com>
2026-02-09 22:38:57 -0300

    Add timing metrics for batch processing

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ DIR_LOGS = os.path.join(DIRETORIO_SISTEMA, "Logs")
 DIR_RELATORIOS = os.path.join(DIRETORIO_SISTEMA, "Relatorios")
 DIR_PRINTS_ERRO = os.path.join(DIR_LOGS, "Prints_Erro")
 
+# Limite (em segundos) para alertar sobre duração longa de executar_sequencia
+LIMITE_TEMPO_SEQUENCIA_SEGUNDOS = 120.0
+
 # Lista de Operadores para a Interface
 LISTA_OPERADORES = [
     "Selecione o Operador",
